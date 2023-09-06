bd1ccaa5b9799a313f29eba16ce7fdf918d4c8de
Fábio <114963708+FabioJro@users.noreply.github.com>
2023-09-06 10:02:34 -0300

    Adjustin some erros

--- src/main/java/com/devsuperior/dslist/entites/Game.java
+++ src/main/java/com/devsuperior/dslist/entites/Game.java
@@ -18,22 +18,28 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plateform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
 
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plateform, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plateform = plateform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -71,12 +77,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlateform() {
-        return plateform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlateform(String plateform) {
-        this.plateform = plateform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
@@ -102,7 +116,6 @@ public class Game {
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
-
+    
+    
 }
-
-
