9c45ad7d861e733f09d6c0982bfd9e72228892c1
Fábio <114963708+FabioJro@users.noreply.github.com>
2023-09-11 08:57:28 -0300

    Sql injection , and querying

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 import org.springframework.transaction.annotation.Transactional;
@@ -33,4 +34,14 @@ public class GameService {
     List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
     return dto;
   }
+   
+  @Transactional(readOnly = true)
+  public List<GameMinDTO> findByList(Long listId) {
+    List<GameMinProjection> result = gameRepository.searchByList(listId);
+    /* Trasnformando uma lista de Game , para o GameMinDto */
+    List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+    return dto;
+  }
+
+
 }
